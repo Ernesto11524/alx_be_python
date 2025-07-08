0b8b3ea2e9f742df383946e7887aca584991897a
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-07-08 19:35:18 +0000

    Introduction to python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum = 10 + 5
+difference = 10 - 5
+product = 10 * 5
+
+print("Addition of 10 and 5 is 15")
+print("Substraction of 10 and 5 is 5")
+print("Multiplication of 10 and 5 is 50")
