be4d9e556c76d21cd04f705d69769483d26bd19b
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-07-08 22:53:32 +0000

    This file calculate the user's monthly savings based on inputted monthly incme and expenses

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,8 @@
-montly_income = int(input("Enter your monthly income: "))
-montly_expenses = int(input("Enter your total montly expenses: "))
-montly_savings = montly_income - montly_expenses
-projected_savings = montly_savings * 12 + (montly_savings * 12 * 0.05)
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total montly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
-print(f"Your montly savings are ${montly_savings}")
+print(f"Your montly savings are ${monthly_savings}")
 print(f"Project savings after one year, with interest is: ${projected_savings}")
 
