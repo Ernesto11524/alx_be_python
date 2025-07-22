08163dacc4be945225d523745be86cfd422a8ac3
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-07-22 11:18:29 +0000

    No description

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,9 +5,9 @@ def perform_operation(num1, num2, operation):
             case 'subtract': result = num1 - num2
             case 'multiply': result = num1 * num2
             case 'divide': 
-                    if (num2 == 0):
+                    if num2 == 0:
                         result = "Mass error"
-                    else:
+                    elif num2 != 0:
                          result = num1 / num2
     else:
          result = "Please enter the right operation(add, subtract, multiply, divide):"
