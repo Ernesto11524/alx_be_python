e0e733ad14ef5b45a9b5b08aea6d6d79a05f3d27
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-07-26 09:37:19 +0000

    No description

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,20 @@
+import datetime
+
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    current_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    current_date = f"Current date and time: {current_date}"
+    return  current_date
+
+print(display_current_datetime())
+
+num = input("Enter the number of days to add to the current date: ")
+num = int(num)
+
+def calculate_future_date():
+    today = datetime.datetime.now().date()
+    future_date = today + datetime.timedelta(days=num)
+    future_date = f"Future date: {future_date}" 
+    return future_date
+
+print(calculate_future_date())
