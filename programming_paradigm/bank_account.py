9396d33e9454f4036af9d7e12a45ef8919658ad7
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-07-29 19:12:29 +0000

    No description

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+class BankAccount:
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance = amount + self.account_balance
+        print(f"New balance is {self.account_balance} GHS")
+    
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            print(f"Withdrawing...... {amount} GHS from your account")
+            current_balance = self.account_balance - amount
+            self.account_balance = current_balance
+            print(f"Remaining balance is {self.account_balance} GHS")
+            return self.account_balance
+        else:
+            print("Insufficient funds. You cannot withdraw more than your current balance.")
+            self.display_balance()
+            return self.account_balance
+        
+    def display_balance(self):
+        print(f"Your current balance is {self.account_balance}")       
